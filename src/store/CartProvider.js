68c15e04ad22c0cdbb9a364224e5dd2fb367fd0a
ashikahmedfahim <41462787+ashikahmedfahim@users.noreply.github.com>
2021-12-31 11:50:49 +0600

    unique items set to single item

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,22 +1,37 @@
-import React, {useReducer} from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
 const CartProvider = (props) => {
   const defaultCartState = {
     items: [],
     totalAmount: 0,
-  }
+  };
   const cartReducer = (state, action) => {
-    if  (action.type === "ADD_ITEM") {
-      const updatedItems = state.items.concat(action.payload);
-      const updatedTotalAmount = state.totalAmount + action.payload.price * action.payload.amount;
+    if (action.type === "ADD_ITEM") {
+      const updatedTotalAmount =
+        state.totalAmount + action.payload.price * action.payload.amount;
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      const existingCartItem = state.items[existingCartItemIndex];
+      let updatedItems;
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.payload.amount,
+        };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems = state.items.concat(action.payload);
+      }
       return {
         items: updatedItems,
         totalAmount: updatedTotalAmount,
-      }
+      };
     }
     return defaultCartState;
-  }
+  };
 
   const [cartState, dispatch] = useReducer(cartReducer, defaultCartState);
 
